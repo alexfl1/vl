7020cc7a3a0d5c3c99a5815a3c73471108518577
alex <alexfl7@ya.ru>
2016-03-02 22:28:18 +0300

    one

---
+++ vlans/base/models.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.utils import timezone
+
+# Create your models here.
+from domains.models import domains
+
+
+class base(models.Model):
+#    author = models.ForeignKey('auth.User')
+#    title = models.CharField(max_length=200)
+#    text = models.TextField()
+#    created_date = models.DateTimeField(
+ #           default=timezone.now)
+#    published_date = models.DateTimeField(
+#            blank=True, null=True)
+#    node_id = models.IntegerField()
+    #name = models.CharField(max_length=256)
+    #created_date = models.DateTimeField(default=timezone.now)
+    #node_id = models.ForeignKey(nodes,default=0)
+    #description = models.TextField()
+    number = models.IntegerField(default=0,)
---
+++ vlans/domains/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import domains
+# Register your models here.
+admin.site.register(domains)
